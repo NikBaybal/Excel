a4eb7c402aac142ef14a5a961e94e41f3b89e3ac
Pavel <nikipparap@mail.ru>
2023-02-24 21:02:50 +0900

    обработка данных шин 110кВ

--- Shiny110kV.py
+++ Shiny110kV.py
@@ -17,32 +17,39 @@ import datetime
 cwd = os.getcwd()
 os.chdir("C:/Users/nikip/PycharmProjects/data processing")
 os.listdir('.')
-file = 'ГТУ3.xlsx'                                                                               # задать имя файла
+file = 'СШ110.xlsx'                                                                               # задать имя файла
 xl = pd.ExcelFile(file)
 diff = datetime.timedelta(hours=9)                                                               # задать разницу во времени
-def data_clean(df_in):
-    time = df_in.columns[0]
-    power = df_in.columns[1]
-    freq = df_in.columns[2]
-    df_obr = df_in[[time, freq, power]]
-    df_obr[time] = pd.to_datetime(df_obr[time]).round('S')
+def data_clean(df_in,num):
+    time = df_in.columns[num]
+    freq = df_in.columns[num+1]
+    df_obr = df_in[[time, freq]]
+    #df_obr[time] = pd.to_datetime(df_obr[time]).round('S')
     df_obr[time] = df_obr[time] - diff
-    df_obr[freq], df_obr[power] = df_obr[freq].map('{:.3f}'.format), df_obr[power].map('{:.2f}'.format)
+    df_obr[freq] = df_obr[freq].map('{:.3f}'.format)
     df_obr = df_obr.drop_duplicates(subset=time, ignore_index=True)
-    df_out = pd.DataFrame(pd.date_range(df_in[time][0] - diff, df_in[time].iloc[-1] - diff, freq="S"), columns=[time])
+    df_out = pd.DataFrame(pd.date_range(df_obr[time][0] - diff, df_obr[time].iloc[-1] - diff, freq="S"), columns=[time])
     df_out = df_out.merge(df_obr, how='left').fillna(method='pad')
     begin_data = df_out[time][0].strftime('%Y%m%d')
     begin_time = df_out[time][0].strftime('%H%M%S')
     df_out[time] = df_out[time].dt.strftime('%Y.%m.%d %H:%M:%S')
     return df_out,begin_data,begin_time
 
-n='01'                                                                                                          # задать номер ГТУ
 name='YAGRESNOVAYA'                                                                                             # задать имя станции
 sheets = xl.sheet_names
-for row in sheets:
-  df_in = xl.parse(row)
-  df_out,begin_data,begin_time = data_clean(df_in)
-  df_out.to_csv(f"{name}.{n}.{begin_data}.{begin_time}.csv",index=False,header=False,sep=';')
+df_in = xl.parse('Лист1')
+df_out, begin_data, begin_time = data_clean(df_in,0)
+print(df_out)
+
+
+
+# for row in sheets:
+#   df_in = xl.parse(row)
+#   for row2 in [0,2,4,6]:
+#       df_out, begin_data, begin_time = data_clean(df_in,row2)
+#       df_out = df_out.merge(df_out, how='left',on=time)
+#   df_out.to_csv(f"{name}.{begin_data}.{begin_time}.csv", index=False, header=False, sep=';')
+
 
 
 
--- test.py
+++ test.py
@@ -1,39 +1,5 @@
-import os
-import pandas as pd
-import openpyxl
-import datetime
-
-# Алгоритм обработки данных
-#- привести все строки со временем к формату времени
-#- убрать дубликаты
-#- привести к UTC
-#- создать новый столбец с временем без пропусков
-#- заполнить данными исходную таблицу
-#- обработать все листы
-#- создать файл csv с требуемым названием
-
-cwd = os.getcwd()                                                               # Retrieve current working directory (`cwd`)
-os.chdir("C:/Users/nikip/PycharmProjects/data processing")                      # Change directory
-os.listdir('.')                                                                 # List all files and directories in current directory
-file = 'ГТУ3.xlsx'
-xl = pd.ExcelFile(file)
-df_in = xl.parse('Лист1')
-time = df_in.columns[0]
-diff = datetime.timedelta(hours=9)
-def data_clean(df_in):
-    time = df_in.columns[0]
-    power = df_in.columns[1]
-    freq = df_in.columns[2]
-    df_obr = df_in[[time, freq, power]]
-    df_obr[time] = pd.to_datetime(df_obr[time]).round('S')
-    df_obr[time] = df_obr[time] - diff
-    df_obr[freq], df_obr[power] = df_obr[freq].round(3), df_obr[power].round(2)
-    df_obr = df_obr.drop_duplicates(subset=time, ignore_index=True)
-    df_out = pd.DataFrame(pd.date_range(df_in[time][0] - diff, df_in[time].iloc[-1] - diff, freq="S"), columns=[time])
-    df_out = df_out.merge(df_obr, how='left').fillna(method='pad')
-    df_out[time] = df_out[time].dt.strftime('%Y.%m.%d %H:%M:%S')
-
-    return df_out
+for row2 in [0,2,4,6]:
+    print(row2)
 
 
 
